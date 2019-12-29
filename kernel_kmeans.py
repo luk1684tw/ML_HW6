643224188dbfffa7f8055ef16c4bd6f17073e0bc
luk1684tw <luk1684tw@gmail.com>
2019-12-29 23:31:29 +0800

    done

--- kernel_kmeans.py
+++ kernel_kmeans.py
@@ -18,38 +18,29 @@ def make_kernel(x, y, gamma_s, gamma_c):
     return np.dot(np.exp(-gamma_s * dist_spatial), np.exp(-gamma_c * dist_color))
 
 
-def kmeans_plus(k, kernel):
-    mu = np.zeros((k))
-    # if method == 'kmeans++':
-    #     pass
-    # else:
-    rand = np.random.uniform(0, 10000, k)
-    rand = rand.astype(int)
-    print (rand)
-    for i in range(k):
-        mu[i] = kernel[rand[i], rand[i]]
-
-    return mu
-
-
-def plot_res(prd_res, name, clusters):
+def plot_res(prd_res, name, clusters, img):
     prd_res = prd_res.reshape((100, 100))
     plt.imshow(prd_res)
-    plt.savefig(os.path.join(f'./{clusters}/{name}.png'))
+    plt.savefig(os.path.join(f'./tmp/IMG{img}/{clusters}/{name}.png'))
 
     return
 
 
-def kernel_kmeans(img):
+def kernel_kmeans(img, order):
     for gamma_c in Gamma_c:
         for gamma_s in Gamma_s:
             kernel = make_kernel(img, img, gamma_s, gamma_c)
             for k in K:
+                # tmp = list()
+                # for i in range(k-1):
+                #     tmp += [i]*int(10000/k)
+                # tmp += [k-1]*(10000-len(tmp))
+                # prd_res = np.array(tmp)
                 prd_res = np.random.randint(k, size=10000)
                 prd_prev = np.zeros(10000)
                 iteration = 0
                 while not np.array_equal(prd_res, prd_prev) and iteration < 60:
-                    dist = np.tile(np.diag(kernel_im1), k).reshape(-1, k)
+                    dist = np.tile(np.diag(kernel), k).reshape(-1, k)
                     prd_prev = np.copy(prd_res)
                     print ('iteration:', iteration)
                     num_points = np.array([len(prd_res[prd_res==c]) for c in range(k)])
@@ -61,38 +52,17 @@ def kernel_kmeans(img):
                     prd_res = np.argmin(dist, axis=1)
                     iteration += 1
 
-                plot_res(prd_res, f"{gamma_c}_{gamma_s}_{k}", k)
+                    plot_res(prd_res, f"m2_{gamma_c}_{gamma_s}_{k}_ite{iteration}", k, order)
 
     return
 
 
 if __name__ == "__main__":
-    im1 = np.array(Image.open('./image1.png')).reshape((10000, 3))
-    # im2 = np.array(Image.open('./image2.png')).reshape((10000, 3))
+    im1 = np.array(Image.open('./image1.png')).reshape((10000, 3)) / 255
+    im2 = np.array(Image.open('./image2.png')).reshape((10000, 3)) / 255
     
-    Gamma_s, Gamma_c = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1], [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+    Gamma_s, Gamma_c = [0.1], [0.9]
     K = [2, 3, 4, 5]
-    # for gamma_c in Gamma_c:
-    #     for gamma_s in Gamma_s:
-    #         kernel_im1 = kernel(im1, im1, gamma_s, gamma_c)
-    #         # kernel_im2 = kernel(im2, im2, gamma_s, gamma_c)
-    #         for k in K:
-    #             prd_res = np.random.randint(k, size=10000)
-    #             prd_prev = np.zeros(10000)
-    #             iteration = 0
-    #             while not np.array_equal(prd_res, prd_prev) and iteration < 60:
-    #                 dist = np.tile(np.diag(kernel_im1), k).reshape(-1, k)
-    #                 prd_prev = np.copy(prd_res)
-    #                 print ('iteration:', iteration)
-    #                 num_points = np.array([len(prd_res[prd_res==c]) for c in range(k)])
-    #                 print (num_points)
-    #                 for c in range(k):
-    #                     # print (kernel_im1[:, prd_res==c].shape)
-    #                     dist[:, c] -= 2/num_points[c] * np.sum(kernel_im1[:, prd_res==c], axis=1)
-    #                     dist[:, c] += (1/num_points[c])**2 * np.sum(kernel_im1[prd_res==c][:, prd_res==c])
-    #                 prd_res = np.argmin(dist, axis=1)
-    #                 iteration += 1
 
-    #             plot_res(prd_res, f"{gamma_c}_{gamma_s}_{k}", k)
-
-    kernel_kmeans(im1)
+    kernel_kmeans(im1, 1)
+    kernel_kmeans(im2, 2)
--- spectual_cluster.py
+++ spectual_cluster.py
@@ -3,6 +3,7 @@ import os
 from PIL import Image
 from scipy.spatial import distance
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def kernel(x, y, gamma_s, gamma_c):
@@ -18,29 +19,91 @@ def kernel(x, y, gamma_s, gamma_c):
     return np.dot(np.exp(-gamma_s * dist_spatial), np.exp(-gamma_c * dist_color))
 
 
-def spectual_clustering(img):
-    kernel_img = kernel(im1, im1, gamma_s, gamma_c)
-    for i in range(len(kernel_img)):
-        kernel_img[i, i] = 0
+def kmeans(U, k, method, img):
+    tmp = list()
+    for i in range(k-1):
+        tmp += [i]*int(10000/k)
+    tmp += [k-1]*(10000-len(tmp))
+    prd_result = np.array(tmp)
+    # prd_result = np.random.randint(k, size=10000)
+    center = np.zeros((k, k))
+    prev_center = np.ones((k, k))
+    iteration = 0
+    plot_res(prd_result, f'{method}_M1_{k}_{iteration}', k, img)
+    while not np.array_equal(center, prev_center) and iteration < 100:
+        prev_center = np.copy(center)
+        for i in range(k):
+            center[i] = np.mean(U[prd_result == i], axis=0)
+        print ([len(prd_result[prd_result == i]) for i in range(k)])
+        prd_result = np.argmin(distance.cdist(U, center), axis=1)
+        iteration += 1
+        plot_res(prd_result, f'{method}_M1_{k}_{iteration}', k, img)
+    return prd_result
 
-    D = list()
-    for i in range(len(kernel_img)):
-        D.append(np.sum(kernel_img[i,:]))
-    D = np.diag(np.array(D))
+
+def spectual_clustering(img, k, method, img_count):
+    kernel_img = kernel(img, img, gamma_s, gamma_c)
+    D = np.diag(np.sum(kernel_img, axis=1))
+
+    if method == 'normal':
+        D_prime = np.linalg.inv(np.sqrt(D))
+        L = np.dot(D_prime, np.dot(D - kernel_img, D_prime))
+    else:
+        L = D - kernel_img
+
+    print ('Start eigen decomposition')
+    eig_val, eig_vec = np.linalg.eig(L)
+    eigen_dict = dict(zip(eig_val, eig_vec.T))
+    U = np.zeros((10000, k))
+    for i, val in enumerate(np.sort(eig_val)[1:]):
+        print (i, val)
+        if i == k:
+            break
+        else:
+            U[:, i] = eigen_dict[val]
+    for i in range(len(U)):
+        U[i] /= np.linalg.norm(U[i])
+
+    prd_result = kmeans(U, k, method, img_count)
+    # plot_data(U, f'{method}_{k}_{img_count}.png', prd_result, k)
+    return
+
+
+def plot_data(U, name, prd_result, k):
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    if k == 2:
+        for i in range(k):
+            cluster_data = U[prd_result == i]
+            ax.scatter(cluster_data[:, 0], cluster_data[:, 1], c=colors[i])
+    else:
+        for i in range(k):
+            cluster_data = U[prd_result == i]
+            ax.scatter(cluster_data[:, 0], cluster_data[:, 1], cluster_data[:, 2], c=colors[i])
     
-    # normal_cut
-    D_inverse = np.linalg.inv(D)
-    L = np.dot(D_inverse, np.dot(kernel_img, D_inverse))
-    # ratio_cut
-    L = kernel_img
+    plt.savefig(f'./{name}')
+    return
 
+
+def plot_res(prd_res, name, clusters, img):
+    plt.close()
+    prd_res = prd_res.reshape((100, 100))
+    plt.imshow(prd_res)
+    plt.savefig(os.path.join(f'./tmp/IMG{img}/sp/{clusters}/{name}.png'))
 
     return
 
 
 if __name__ == "__main__":
-    im1 = np.array(Image.open('./image1.png')).reshape((10000, 3))
-    # im2 = np.array(Image.open('./image2.png')).reshape((10000, 3))
+    im1 = np.array(Image.open('./image1.png')).reshape((10000, 3)) / 255
+    im2 = np.array(Image.open('./image2.png')).reshape((10000, 3)) / 255
 
-    gamma_s, gamma_c = 1, 1
-    spectual_clustering(im1)
+    colors = ['b', 'g', 'r', 'c', 'm']
+    gamma_s, gamma_c = 0.1, 0.9
+    K = [2, 3, 4, 5]
+    for k in K:
+        spectual_clustering(im1, k, 'normal', 1)
+        spectual_clustering(im1, k, 'ratio', 1)
+
+        spectual_clustering(im2, k, 'normal', 2)
+        spectual_clustering(im2, k, 'ratio', 2)
